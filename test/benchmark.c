842db1a91ef3dca0c18b2b14851c17a170ec93ee
Nicolas Couvrat <nicolas.couvrat@mines-paristech.fr>
2017-12-27 15:58:24 +0900

    benchmark

---
+++ test/benchmark.c
@@ -0,0 +1,30 @@
+#include <time.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+#include "../src/index.c"
+#define TEST_STRING "{\"key1\":\"value1\",\"key2\":12345}"
+#define LEN_TEST_STRING 36
+
+int test_run(int number_of_runs) {
+  int i;
+  for (i = 0; i < number_of_runs; i++) {
+    int len = json_size(TEST_STRING, LEN_TEST_STRING);
+    if (len == -1)
+      exit(EXIT_FAILURE);
+    json_organism_t *organism = json_organism_initialize(len, TEST_STRING, LEN_TEST_STRING);
+    if (json_organism_populate(organism) != 0)
+      exit(EXIT_FAILURE);
+    json_organism_destroy(organism);
+  }
+}
+
+int main() {
+  float startTime = (float)clock()/CLOCKS_PER_SEC;
+  int n_runs = 10000000;
+  test_run(n_runs);
+  float endTime = (float)clock()/CLOCKS_PER_SEC;
+  float timeElapsed = endTime - startTime;
+
+  printf("Time taken for %d runs: %.6f\n", n_runs, timeElapsed);
+}
